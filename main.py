c8ea9d73a31b9f5e22e6c57ed839b30e47ad0281
terryzhang1015 <terryzhang1015@outlook.com>
2025-03-16 17:38:07 +0800

    Refactor code

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-from prompt_generator import PromptGenerator
+from scn.decompiler import SCNDecompiler
+from dialogue.bin_generator import DialogueBinGenerator
+from prompt.text_collector import PromptTextCollector
+
+class PromptGenerator:
+    def __init__(self, scn_dir, temp_dir, output_dir):
+        self.decompiler = SCNDecompiler(scn_dir, temp_dir)
+        self.bin_generator = DialogueBinGenerator(scn_dir, temp_dir)
+        self.text_processor = PromptTextCollector(temp_dir, output_dir)
+
+    def generate(self):
+        self.decompiler.decompile_scn()
+        print('==========SCN DECOMPILATION DONE===========')
+        self.bin_generator.generate()
+        print('==========DIALOGUE BIN CREATED===========')
+        self.text_processor.process()
+        print('==========PROMPT CSV GENERATED===========')
+
 
 SCN_DIR = 'scn_files'
 TEMP_DIR = 'temp'
