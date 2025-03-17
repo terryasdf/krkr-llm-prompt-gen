702b090a544282b43198232f6a222d2254cc1eeb
terryzhang1015 <terryzhang1015@outlook.com>
2025-03-17 13:18:14 +0800

    Implement temp folder clearing before bin gen

--- dialogue/bin_generator.py
+++ dialogue/bin_generator.py
@@ -1,6 +1,7 @@
 from dialogue.extractor import DialogueExtractor
 
 import os
+from shutil import rmtree
 import glob
 
 class DialogueBinGenerator:
@@ -9,6 +10,10 @@ class DialogueBinGenerator:
         self.dst_dir = dst_dir
 
     def generate(self):
+        if os.path.exists(self.dst_dir):
+            rmtree(self.dst_dir)
+        os.mkdir(self.dst_dir)
+
         for file in glob.iglob(f'{self.json_dir}/*.resx.json'):
             os.remove(file)
         for file in glob.iglob(f'{self.json_dir}/*.json'):
